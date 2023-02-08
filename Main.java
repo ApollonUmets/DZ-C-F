b63bf18213ee64d3b7afbd8e38c2d7d8ff0c6d4f
ApollonUmets <123029502+ApollonUmets@users.noreply.github.com>
2023-02-08 23:17:51 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = 32;
+        double b = 1.8;
+        double c;
+        c = scanner.nextDouble();
+        double result = c * b + a;
+        System.out.println("Ваш результат " + result);
+    }
+}
